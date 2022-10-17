58efcb6c7e01fd2f37027b74549ee7a1005e0d42
Heupoxupypr <vasyakus@gmail.com>
2022-10-17 22:45:03 +0300

    Add Task 6 to Homework lesson 1

---
+++ Homework_lesson_1/Task_6/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Write("Please enter number: ");
+string numberStr = Console.ReadLine();
+
+int number = int.Parse(numberStr);
+
+if ((number % 2) == 0)
+{
+    Console.WriteLine("Number is EVEN!");
+}
+else
+{
+    Console.WriteLine("Number is ODD!");
+}
