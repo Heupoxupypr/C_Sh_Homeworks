3cdb83d04c8e6e8e68e3b61886567086ca9c3671
Heupoxupypr <vasyakus@gmail.com>
2022-10-17 22:10:47 +0300

    Add Task 2 to Homework Lesson 1

---
+++ Homework_lesson_1/Task_2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Write ("Please enter first number: ");
+string number_1Str = Console.ReadLine();
+Console.Write ("Please enter second number: ");
+string number_2Str = Console.ReadLine();
+
+int number_1 = int.Parse(number_1Str);
+int number_2 = int.Parse(number_2Str);
+
+if (number_1 == number_2)
+{
+    Console.WriteLine ("Both numbers are equal");
+}
+else if (number_1 > number_2)
+{
+    Console.WriteLine ($"{number_1} greater than the {number_2}");
+}
+else
+{
+    Console.WriteLine ($"{number_2} greater than the {number_1}");
+}
