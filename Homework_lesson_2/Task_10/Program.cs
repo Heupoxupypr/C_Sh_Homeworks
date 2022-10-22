9662758f4b1bd171bb82c764dae301a37274a9ff
Heupoxupypr <vasyakus@gmail.com>
2022-10-22 14:38:42 +0300

    Add Task 10

---
+++ Homework_lesson_2/Task_10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+            //     456 -> 5
+            //     782 -> 8
+            //     918 -> 1
+
+            Console.Write ("Please enter number: ");
+            int number = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine ($"Third digit of the number {number} is {number / 10 % 10}");
+           
+        }
+    }
+}
