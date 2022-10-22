461513963fcacf7ef9a58c90d38e46d04b8b4eac
Heupoxupypr <vasyakus@gmail.com>
2022-10-22 14:52:18 +0300

    Add Task 13

---
+++ Homework_lesson_2/Task_13/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write ("Please enter number: ");
+int number = int.Parse(Console.ReadLine()) /100;
+
+if (number > 0 )
+    Console.WriteLine ($"Third digit of the number {number % 10}");
+else 
+    Console.WriteLine ($"Third digit of the number is NOT");
