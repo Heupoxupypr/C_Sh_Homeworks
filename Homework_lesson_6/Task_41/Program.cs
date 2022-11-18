6d103c63dcba8c3c488961234110f390b2fc3a0b
Heupoxupypr <vasyakus@gmail.com>
2022-11-18 04:48:14 +0300

    Add Task 41

---
+++ Homework_lesson_6/Task_41/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+//Функция получения от пользователя чисел
+int GetDigits(string message)
+{
+    int PositiveCount = 0;
+    Console.WriteLine(message);
+    int m = int.Parse(Console.ReadLine() ?? "");
+
+    Console.WriteLine($"Please enter {m} numbers: ");
+
+    for (int i = 0; i < m; i++)
+    {
+        if (int.Parse(Console.ReadLine() ?? "") > 0)
+            PositiveCount++;
+    }
+    return PositiveCount;
+}
+
+Console.WriteLine($"User enter {GetDigits("Please enter count of numbers to enter")} positive numbers.");
