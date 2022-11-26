11ef660a3ee77eebbd68ba96a492785eac9a2e2e
Heupoxupypr <vasyakus@gmail.com>
2022-11-26 06:30:18 +0300

    Add task 52

---
+++ Homework_lesson_7/Task_52/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] InitMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+
+    return matrix;
+}
+
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine()??"");
+    return result;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ColumnArithmeticMean(int[,] matrix)
+{
+    double aritmean = 0;
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            aritmean += matrix[i, j];
+        }
+        Console.Write($"{Math.Round(aritmean / matrix.GetLength(0), 2)}\t");
+        aritmean = 0;
+    }
+    Console.WriteLine();
+}
+
+int m = GetNumber("Введите число m:");
+int n = GetNumber("Введите число n:");
+int[,] matrix = InitMatrix(m, n);
+PrintMatrix(matrix);
+ColumnArithmeticMean(matrix);
