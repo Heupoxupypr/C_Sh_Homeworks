39edf0a6091c907dd088373e19054e48a4fa65af
Heupoxupypr <vasyakus@gmail.com>
2022-11-26 06:18:40 +0300

    Add task 50

---
+++ Homework_lesson_7/Task_50/Program.cs
@@ -0,0 +1,62 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
+int[,] InitMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+        }
+    }
+
+    return matrix;
+}
+
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    int result = int.Parse(Console.ReadLine()??"");
+    return result;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int m = GetNumber("Введите число m:");
+int n = GetNumber("Введите число n:");
+int[,] matrix = InitMatrix(m, n);
+PrintMatrix(matrix);
+
+int num = GetNumber("Введите номер элемента для поиска");
+
+//Считаем что нумерация идет от 0
+if (num >= (n * m))
+{
+    Console.WriteLine($"Элемента под номером {num} не существует в данной матрице");
+}
+else
+{
+    int x = num / n;
+    int y = num - n * x;
+    Console.WriteLine($"x = {x}\ty = {y}");
+    Console.WriteLine($"Элемента под номером {num} равен {matrix[x, y]}");
+}
